aa1617d5e9f3adb2a2689d1d6ff850c659377c96
Dominique Hansen <DominiqueHansen@gmx.net>
2024-04-22 15:00:49 +0200

    Add year range slider and group dropdown factory

---
+++ statistics_server/layout.py
@@ -0,0 +1,36 @@
+"""Modular assembling of html layout."""
+
+from typing import Literal, Union
+
+from dash import dcc, html
+
+LABEL_KEY: dict[str, Union[Literal["label"], Literal["label_de"]]] = {
+    "en": "label",
+    "de": "label_de",
+}
+UNSELECTED_VALUE = {
+    "en": {"label": "No Grouping", "value": None},
+    "de": {"label": "Keine Gruppierung", "value": None},
+}
+
+
+def year_range_slider(start_year: int, end_year: int) -> dcc.Slider:
+    """Create a year range Slider."""
+    return dcc.Slider(
+        start_year, end_year, step=1, value=[start_year, end_year], id="year-range-slider"
+    )
+
+
+def grouping_dropdown(metadata, element_id, exclude_value=None, language="en"):
+    """Create a dropdown to select a group to group by."""
+    if language != "en":
+        language = "de"
+    label = LABEL_KEY[language]
+    default = UNSELECTED_VALUE[language]
+    options = [default]
+
+    for group in metadata["groups"]:
+        if exclude_value == group["variable"]:
+            continue
+        options.append({"label": group[label], "value": group["variable"]})
+    return dcc.Dropdown(options=options, value=default["value"], id=element_id)
