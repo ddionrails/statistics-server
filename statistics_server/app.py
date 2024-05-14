adc796bb062f676246bd115563e2cdd41bbc6675
hansendx <dhansen@diw.de>
2024-05-14 14:24:57 +0200

    Add test code to app module

--- statistics_server/app.py
+++ statistics_server/app.py
@@ -1,10 +1,25 @@
+from pathlib import Path
+
 import flask
 from dash import Dash, html
+from json import load
+
+from statistics_server.layout import grouping_dropdown, year_range_slider
+
+group_test_file = Path(".").joinpath("tests/test_data/group_metadata.json").absolute()
 
 server = flask.Flask(__name__)
 app = Dash(__name__, server=server)  # type: ignore
+with open(group_test_file, "r", encoding="utf-8") as file:
+    metadata = load(file)
 
-app.layout = html.Div(["Hello World!"])
+app.layout = html.Div(
+    [
+        "Hello World!",
+        year_range_slider(1999, 2020),
+        grouping_dropdown(metadata=metadata, element_id="test", language="de"),
+    ]
+)
 
 
 def run():
@@ -15,4 +30,4 @@ application = app.run_server
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
