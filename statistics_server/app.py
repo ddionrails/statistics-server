325a549fe8f800bf3f50c8b9ce8bf19611cdd010
hansendx <dhansen@diw.de>
2024-05-17 12:43:45 +0200

    Add more interaction functionality

--- statistics_server/app.py
+++ statistics_server/app.py
@@ -1,35 +1,23 @@
 from json import load
 from pathlib import Path
 
-import flask
 from dash import Dash, Input, Output, callback, dcc, html
+from flask import Flask, request
 from pandas import read_csv
+from plotly.graph_objects import Figure
+from urllib.parse import parse_qs
 
 from statistics_server.layout import grouping_dropdown, year_range_slider
 from statistics_server.simple_graph import create_line_graph_figure
 
 # TODO: remove hardcoding
-variable_name = "years_injob"
-variable_type = "numerical"
 
 data_base_test_path = Path(".").joinpath("tests/test_data/").absolute()
 group_metadata_file = data_base_test_path.joinpath("group_metadata.json").absolute()
 
-data_test_path = data_base_test_path.joinpath(
-    f"{variable_type}/{variable_name}/"
-).absolute()
-data_test_file = data_test_path.joinpath(f"{variable_name}_year.csv").absolute()
-variable_metadata_file = data_test_path.joinpath("meta.json")
-
-with open(variable_metadata_file, "r", encoding="utf-8") as file:
-    variable_metadata = load(file)
-
-
-data_test_frame = read_csv(data_test_file)
-
 # TODO: End remove hardcoding
 
-server = flask.Flask(__name__)
+server = Flask(__name__)
 app = Dash(__name__, server=server)  # type: ignore
 
 with open(group_metadata_file, "r", encoding="utf-8") as file:
@@ -38,7 +26,7 @@ with open(group_metadata_file, "r", encoding="utf-8") as file:
 app.layout = html.Div(
     id="outer-container",
     children=[
-        year_range_slider(variable_metadata["start_year"], variable_metadata["end_year"]),
+        html.Div(id="year-range-slider-container", children=[]),
         html.Div(
             id="control-and-graph",
             children=[
@@ -58,31 +46,105 @@ app.layout = html.Div(
                                 ),
                             ],
                         ),
+                        dcc.Checklist(
+                            id="confidence-checkbox",
+                            options=[
+                                {
+                                    "label": "Show Confidence Interval",
+                                    "value": "confidence",
+                                },
+                            ],
+                            value=["confidence"],
+                        ),
                     ],
                 ),
                 dcc.Graph(
                     id="graph",
                     className="graph-container",
-                    figure=create_line_graph_figure(data_test_frame, group=[]),
+                    figure=Figure(),
                 ),
             ],
         ),
         dcc.Checklist(id="control-panel-checkbox", options=["Hide Control Panel"]),
+        dcc.Location(id="url"),
     ],
 )
+
+
+@callback(
+    Output("year-range-slider-container", "children"),
+    Input("url", "search"),
+)
+def update_range_slider():
+    if search[0] == "?":
+        search = search[1:]
+    parsed_search = parse_qs(search)
+    variable_name = parsed_search["variable"][0]
+    variable_type = parsed_search["type"][0]
+    data_base_path = get_file_names(variable_type, variable_name)
+
+    variable_metadata_file_path = data_base_path.joinpath("meta.json")
+    with open(variable_metadata_file_path, "r", encoding="utf-8") as file:
+        variable_metadata = load(file)
+
+    return (
+        year_range_slider(variable_metadata["start_year"], variable_metadata["end_year"]),
+    )
 
 
 @callback(
     Output("graph", "figure"),
     Output("second-group", "value"),
     Output("second-group", "options"),
+    Input("url", "search"),
     Input("first-group", "value"),
     Input("second-group", "value"),
     Input("first-group", "options"),
-    prevent_initial_call=True,
+    Input("confidence-checkbox", "value"),
 )
-def handle_dropdown(first_group, second_group, first_group_options):
-    # TODO: Handle when all dropdowns are deselected
+def handle_inputs(
+    search, first_group, second_group, first_group_options, show_confidence
+):
+    if search[0] == "?":
+        search = search[1:]
+    parsed_search = parse_qs(search)
+    variable_name = parsed_search["variable"][0]
+    variable_type = parsed_search["type"][0]
+    data_base_path = get_file_names(variable_type, variable_name)
+
+    grouping, options = handle_grouping(first_group, second_group, first_group_options)
+    data_file = data_base_path.joinpath(
+        "_".join([variable_name, "year", *grouping]) + ".csv"
+    ).absolute()
+    _dataframe = read_csv(data_file)
+    measure = "mean"
+    if variable_type == "categorical":
+        grouping.append(variable_name)
+        measure = "proportion"
+
+    return (
+        create_line_graph_figure(
+            _dataframe,
+            group=grouping,
+            show_confidence=bool(show_confidence),
+            measure=measure,
+        ),
+        second_group,
+        options,
+    )
+
+
+def get_file_names(variable_type, variable_name):
+
+    data_base_path = data_base_test_path.joinpath(
+        f"{variable_type}/{variable_name}/"
+    ).absolute()
+
+    return data_base_path
+
+
+def handle_grouping(first_group, second_group, first_group_options):
+
     grouping = []
     if first_group == second_group:
         second_group = None
@@ -92,18 +154,14 @@ def handle_dropdown(first_group, second_group, first_group_options):
     if second_group:
         grouping.append(second_group)
     grouping.sort()
-    grouping_string = "_".join(grouping)
-    data_file = data_test_path.joinpath(
-        f"{variable_name}_year_{grouping_string}.csv"
-    ).absolute()
-    _dataframe = read_csv(data_file)
     options = []
 
     for option in first_group_options:
         if option["value"] is not None and option["value"] == first_group:
             continue
         options.append(option)
-    return create_line_graph_figure(_dataframe, group=grouping), second_group, options
+
+    return grouping, options
 
 
 def run():
